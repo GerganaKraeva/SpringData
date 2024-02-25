dfe2df8260593e9502b0e13c344378d32ca0d5dc
GerganaKraeva <gergana.evg.kraeva@gmail.com>
2024-02-26 00:06:18 +0200

    solution

---
+++ IntroductionToHibernate/Hibernate/src/main/java/Main.java
@@ -0,0 +1,35 @@
+import entities.Student;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+        Configuration configuration=new Configuration();
+        configuration.configure();
+
+        SessionFactory sessionFactory=configuration.buildSessionFactory();
+       Session session= sessionFactory.openSession();
+
+       session.beginTransaction();
+
+//        Student student=new Student();
+//        student.setName("Pesho");
+//        session.save(student);
+
+//        Student student = session.get(Student.class,1);
+//        System.out.printf("%d - %s", student.getId(), student.getName());
+//
+//        student.setName("Updated");
+//
+        List <Student> students = session.createQuery("FROM Student", Student.class).list();
+        for (Student currentStudent : students) {
+            System.out.println(currentStudent);
+        }
+
+       session.getTransaction().commit();
+       session.close();
+    }
+}
